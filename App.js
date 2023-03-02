3b79ba95bf2d22527fb409bbc9a43e7c10cdebee
Ricardo-DSS <ricardo.350.rs@gmail.com>
2023-03-02 17:01:55 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [usuario, setText] = useState('');
+  const handleTextChange = (inputText) => {
+    setText(inputText);
+  };
+
+  const [senha, setText2] = useState('');
+  const handleTextChange2 = (inputText) => {
+    setText2(inputText);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('../linko/assets/logo-sem-fundo.png')}
+        style={{width: 150, height: 150}}
+      />
+      <TextInput
+        style={{ 
+          marginTop: 40,
+          height: 40, 
+          width: 300, 
+          borderColor: 'darkblue', 
+          borderWidth: 1,
+          borderRadius: 10,
+        }}
+        onChangeText={handleTextChange}
+        value={usuario}
+        placeholder = 'Nome de usuário'
+      />
+      <TextInput
+        style={{ 
+          marginTop: 10,
+          marginBottom: 50,
+          height: 40, 
+          width: 300, 
+          borderColor: 'darkblue', 
+          borderWidth: 1,
+          borderRadius: 10,
+        }}
+        onChangeText={handleTextChange2}
+        value={senha}
+        placeholder = 'Senha'
+        secureTextEntry = {true}
+      />
+      <View style={{marginVertical: 5, width: 150}}>
+        <Button
+            title='Entrar' onPress={() => {}}
+            color ='blue'
+        />
+      </View>
+      <View style={{marginVertical: 5, width: 150}}>
+        <Button
+        title='Cadastrar' onPress={() => {}}
+        color ='blue'
+        />
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
